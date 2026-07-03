f578faa89d4a1263a441170254dc94743f156ccd
pgloc <przemekgloc@gmail.com>
2026-07-03 12:52:41 +0200

    Projekt inwazja obcych - statek kosmiczny ver3

---
+++ Python projects/Ćwiczenia/Gra/space_ship_bullet.py
@@ -0,0 +1,30 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+class SpaceShipBullet(Sprite):
+    """Klasa przeznaczona do zarządzania pociskami wystrzeliwanymi przez statek."""
+
+    def __init__(self, ai_game):
+        """Utworzenie obiektu pocisku w aktualnym położeniu statku."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.color = (60, 60, 60)
+
+        # Utworzenie prostokąta pocisku w punkcie (0, 0), a następnie zdefiniowanie dla niego odpowiedniego położenia.
+        self.rect = pygame.Rect(0, 0, 15, 3)
+        self.rect.midleft = ai_game.space_ship.rect.midright
+
+        # Położenie pocisku jest zdefiniowane za pomocą wartości zmiennoprzecinkowej.
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """Poruszanie pociskiem po ekranie."""
+        # Uaktualnienie położenia pocisku.
+        self.x += 1.0
+        # Uaktualnienie położenia prostokąta pocisku.
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        """Wyświetlenie pocisku na ekranie."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- Python projects/Ćwiczenia/Gra/space_ship_game.py
+++ Python projects/Ćwiczenia/Gra/space_ship_game.py
@@ -1,33 +1,33 @@
 import sys
 import pygame
 
-from settings import Settings
-from ship import Ship
-from bullet import Bullet
+from space_ship import SpaceShip
+from space_ship_bullet import SpaceShipBullet
 
-class AlienInvasion:
+class SpaceShipGame:
     """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry."""
 
     def __init__(self):
         """Inicjalizacja gry i utworzenie jej zasobów."""
         pygame.init()
-        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen_rect = self.screen.get_rect()
+        self.bg_color = (230, 230, 230)
         # Fullscreen.
         #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         #self.settings.screen_width = self.screen.get_rect().width
         #self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Inwazja obcych")
+        pygame.display.set_caption("Statek kosmiczny")
 
-        self.ship = Ship(self)
-        self.bullets = pygame.sprite.Group()
+        self.space_ship = SpaceShip(self)
+        self.space_ship_bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Rozpoczęcie pętli głównej gry."""
         while True:
             self._check_events()
-            self.ship.update()
+            self.space_ship.update()
             self._update_bullets()
             self._update_screen()
             
@@ -46,10 +46,10 @@ class AlienInvasion:
 
     def _check_keydown_events(self, event):
         """Reakcja na naciśnięcie klawisza."""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+        if event.key == pygame.K_UP:
+            self.space_ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.space_ship.moving_down = True
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
@@ -57,33 +57,32 @@ class AlienInvasion:
 
     def _check_keyup_events(self, event):
         """Reakcja na zwolnienie klawisza."""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.space_ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.space_ship.moving_down = False
 
     def _fire_bullet(self):
         """Utworzenie nowego pocisku i dodanie go do grupy pocisków."""
-        if len(self.bullets) < self.settings.bullets_allowed:
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
+        if len(self.space_ship_bullets) < 3:
+            new_bullet = SpaceShipBullet(self)
+            self.space_ship_bullets.add(new_bullet)
 
     def _update_bullets(self):
         """Uaktualnienie położenia pocisków i usunięcie tych niewidocznych na ekranie."""
         # Uaktualnienie położenia pocisków.
-        self.bullets.update()
+        self.space_ship_bullets.update()
 
         # Usunięcie pocisków, które znajdują się poza ekranem.
-        for bullet in self.bullets.copy():
-            if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet)
-
+        for bullet in self.space_ship_bullets.copy():
+            if bullet.rect.left >= self.screen_rect.right:
+                self.space_ship_bullets.remove(bullet)
 
     def _update_screen(self):
         """Uaktualnienie obrazów na ekranie i przejście do nowego ekranu."""
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-        for bullet in self.bullets.sprites():
+        self.screen.fill(self.bg_color)
+        self.space_ship.blitme()
+        for bullet in self.space_ship_bullets.sprites():
             bullet.draw_bullet()
 
         # Wyświetlenie ostatnio zmodyfikowanego ekranu.
@@ -91,5 +90,5 @@ class AlienInvasion:
 
 if __name__ == '__main__':
     # Utworzenie egzemplarza gry i jej uruchomienie.
-    ai = AlienInvasion()
+    ai = SpaceShipGame()
     ai.run_game()
